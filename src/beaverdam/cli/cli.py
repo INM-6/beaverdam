65252d3af9176e222564a71a940267371ab7f69b
Heather More <heather@hlmore.com>
2024-04-10 14:25:56 +0200

    Make autocomplete faster
    Autocompleting the commands was insanely slow.  Putting imports
    inside the commands helped.  I got the idea from this issue:
    https://github.com/tiangolo/typer/issues/231

--- src/beaverdam/cli/cli.py
+++ src/beaverdam/cli/cli.py
@@ -1,19 +1,22 @@
 import typer
 
-from beaverdam.builder import build_database
-from beaverdam.viewer import run_ui
-
-# Create subcommand apps
-# build = typer.Typer()
-# view = typer.Typer()
 
 # Create main app
+
 app = typer.Typer()
+
+# Add commands for each functionality
+
 
 @app.command()
 def build(config_file_name):
+    from beaverdam.builder import build_database
+
     build_database(config_file_name)
+
 
 @app.command()
 def view(config_file_name):
+    from beaverdam.viewer import run_ui
+
     run_ui(config_file_name)
